12fdac3dd78cc6c623d447876ae9e88ffefdb071
jetSett <joel.felderhoff@ens-lyon.fr>
2019-09-08 15:20:14 +0200

    base commit

---
+++ base.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define FOR(i, n) for(int i=0; i<n; ++i)
+#define ALL(v) v.begin(),v.end()
+#define D(x) cout << (x) << endl;
+#define Dn(x) cout << #x <<" : " << (x) << endl;
+using LL = int64_t;
+using ULL = uint64_t;
+using namespace std;
+typedef vector<LL> VI; typedef vector<VI> VVI;
+
+int main(){
+    ios::sync_with_stdio(false);
+
+    return 0;
+}
---
+++ build.sh
@@ -0,0 +1,1 @@
+g++ -std=c++14 -Wall $1 -o out
